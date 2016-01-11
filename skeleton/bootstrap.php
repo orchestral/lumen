c76c6be391f0bbb690a8de2522c181598f6841bf
crynobone <crynobone@gmail.com>
2016-01-11 21:01:15 +0800

    Fixes namespace.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- skeleton/bootstrap.php
+++ skeleton/bootstrap.php
@@ -81,9 +81,9 @@ $app->singleton(
 
 // $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 // $app->register(Dingo\Api\Provider\LumenServiceProvider::class);
-// $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
+// $app->register(App\Lumen\Providers\AppServiceProvider::class);
+// $app->register(App\Lumen\Providers\AuthServiceProvider::class);
+// $app->register(App\Lumen\Providers\EventServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
