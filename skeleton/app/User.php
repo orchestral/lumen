63c737fe4f091836b5057598f1dccc1a40522cf8
crynobone <crynobone@gmail.com>
2016-01-16 16:14:12 +0800

    Add App\Lumen\User.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ skeleton/app/User.php
@@ -0,0 +1,31 @@
+<?php namespace App\Lumen;
+
+use Orchestra\Model\User as Eloquent;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+
+class User extends Eloquent implements AuthorizableContract, JWTSubject
+{
+    use Authorizable;
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
