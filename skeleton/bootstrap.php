216abd1eca15e2c2d2fef54483eb106aa7c8da2b
crynobone <crynobone@gmail.com>
2016-01-16 15:51:38 +0800

    Simplify comments.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- skeleton/bootstrap.php
+++ skeleton/bootstrap.php
@@ -56,11 +56,11 @@ $app->singleton(
 */
 
 // $app->middleware([
-//     // Illuminate\Cookie\Middleware\EncryptCookies::class,
-//     // Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-//     // Illuminate\Session\Middleware\StartSession::class,
-//     // Illuminate\View\Middleware\ShareErrorsFromSession::class,
-//     // Laravel\Lumen\Http\Middleware\VerifyCsrfToken::class,
+//     Illuminate\Cookie\Middleware\EncryptCookies::class,
+//     Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+//     Illuminate\Session\Middleware\StartSession::class,
+//     Illuminate\View\Middleware\ShareErrorsFromSession::class,
+//     Laravel\Lumen\Http\Middleware\VerifyCsrfToken::class,
 // ]);
 
 // $app->routeMiddleware([
