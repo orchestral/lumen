026a4fcaf5c934ed0bf64d58db20e4a8bd57a53a
crynobone <crynobone@gmail.com>
2016-04-09 11:05:57 +0800

    Add Api Token auth provider.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Auth/Providers/ApiToken.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Laravel\Lumen\Auth\Providers;
+
+use Dingo\Api\Routing\Route;
+use Illuminate\Http\Request;
+use Orchestra\Auth\AuthManager;
+use Dingo\Api\Auth\Provider\Authorization;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
+class ApiToken extends Authorization
+{
+    /**
+     * Illuminate authentication manager.
+     *
+     * @var \Orchestra\Auth\AuthManager
+     */
+    protected $auth;
+
+    /**
+     * Create a new basic provider instance.
+     *
+     * @param \Orchestra\Auth\AuthManager $auth
+     */
+    public function __construct(AuthManager $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Authenticate request with a JWT.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Dingo\Api\Routing\Route $route
+     *
+     * @return mixed
+     */
+    public function authenticate(Request $request, Route $route)
+    {
+        if (! $user = $this->auth->guard('api')->user()) {
+            throw new UnauthorizedHttpException('ApiToken', 'Unable to authenticate with invalid API key and token.');
+        }
+
+        return $user;
+    }
+
+    /**
+     * Get the providers authorization method.
+     *
+     * Note: Not used directly, added just for contract requirement.
+     *
+     * @return string
+     */
+    public function getAuthorizationMethod()
+    {
+        return 'API_TOKEN';
+    }
+}
