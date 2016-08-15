0865b688510d336c0e43ef837de2613a0e48ceb9
crynobone <crynobone@gmail.com>
2016-08-15 19:23:07 +0800

    Add transformWith and serializeWith.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Routing/Helpers.php
+++ src/Routing/Helpers.php
@@ -18,16 +18,48 @@ trait Helpers
      */
     protected function transform($instance, $name)
     {
-        $version     = $this->getVersionNamespace();
+        return $this->serializeWith($this->transformWith($instance, $name), $name);
+    }
+
+    /**
+     * Transform the instance.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model|\Illuminate\Support\Collection  $instance
+     * @param  string  $name
+     *
+     * @return mixed
+     */
+    protected function transformWith($instance, $name)
+    {
+        $version = $this->getVersionNamespace();
         $transformer = "{$this->namespace}\\Transformers\\{$version}\\{$name}";
-        $serializer  = "{$this->namespace}\\Serializers\\{$version}\\{$name}";
 
         if (class_exists($transformer)) {
-            $instance = $instance->transform(app($transformer));
+            return $instance->transform(app($transformer));
         }
+
+        return $instance;
+    }
+
+    /**
+     * Transform the instance.
+     *
+     * @param  mixed  $instance
+     * @param  string  $name
+     *
+     * @return mixed
+     */
+    protected function serializeWith($instance, $name)
+    {
+        $version = $this->getVersionNamespace();
+        $serializer = "{$this->namespace}\\Serializers\\{$version}\\{$name}";
 
         if (class_exists($serializer)) {
             return call_user_func(app($serializer), $instance);
+        }
+
+        if ($name !== 'Serializer') {
+            return $this->serializeWith($instance, 'Serializer');
         }
 
         return $instance;
