db92e1802b74021fabae73347286303fa8faa349
crynobone <crynobone@gmail.com>
2017-09-16 12:20:02 +0800

    Remove data_get usage.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Application.php
+++ src/Application.php
@@ -651,7 +651,7 @@ class Application extends Container implements ApplicationContract
 
         $composer = json_decode(file_get_contents($this->basePath('composer.json')), true);
 
-        foreach ((array) data_get($composer, 'autoload.psr-4') as $namespace => $path) {
+        foreach ((array) ($composer['autoload']['psr-4'] ?? []) as $namespace => $path) {
             foreach ((array) $path as $pathChoice) {
                 if (realpath($this->path()) == realpath($this->basePath().'/'.$pathChoice)) {
                     return $this->namespace = $namespace;
